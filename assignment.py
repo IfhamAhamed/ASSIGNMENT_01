9f6d8a06fa8e0874d2749aefe45ac0c54c881291
IfhamAhamed <57429027+IfhamAhamed@users.noreply.github.com>
2021-01-28 12:35:35 +0530

    2# commit

--- assignment.py
+++ assignment.py
@@ -3,56 +3,67 @@ import json
 import re
 import os
 
-def validate_record (req_url):
-    file = open('config.json')
-    data = json.load(f)
+class Check_url:
 
-    for i in data['attack_check']:
-        if re.search("", req_url):
-            return True
-            break
+    gtime = ''
 
-def eval_line (last_line):
-    # 192.168.67.128 - - [20/Jan/2021:22:14:12 +0530] "-" 408 0 "-" "-"
-    tmp = last_line.split(' ')
-    ip = tmp[0]
-    t = tmp[3]
-    print(ip)
-    print(time)
+    def validate_record (self, req_url):
+        file = open('config.json')
+        data = json.load(f)
 
+        for i in data['attack_check']:
+            if re.search("*id*", req_url):
+                return True
+                break
+
+    def eval_line (self, last_line):
+        # 192.168.67.128 - - [20/Jan/2021:22:14:12 +0530] "-" 408 0 "-" "-"
+        tmp = last_line.split(' ')
+        ip = tmp[0]
+        url = tmp[6]
+        tim = tmp[3].split(':', 1)[1]
+        
+        #print(self.gtime, tim)
+        #print(tmp)
+        if self.gtime != tim:
+            # this is a new record
+            self.gtime = tim
+            print(self.validate_record(url))
+
+
+    def find_last (self, fname, N):
+
+        bufsize = 8192  
+        fsize = os.stat(fname).st_size
+        iter = 0
+
+        with open(fname) as f: 
+            if bufsize > fsize:        
+                bufsize = fsize-1
+                fetched_lines = [] 
+
+                while True: 
+                    iter += 1
+                    f.seek(fsize-bufsize * iter) 
+                    fetched_lines.extend(f.readlines()) 
     
-
-
-def find_last (fname, N):
-
-    bufsize = 8192  
-    fsize = os.stat(fname).st_size
-    iter = 0
-
-    with open(fname) as f: 
-        if bufsize > fsize:        
-            bufsize = fsize-1
-            fetched_lines = [] 
-
-            while True: 
-                iter += 1
-                f.seek(fsize-bufsize * iter) 
-                fetched_lines.extend(f.readlines()) 
- 
-                if len(fetched_lines) >= N or f.tell() == 0: 
-                        # print() 
-                        line = ''.join(fetched_lines[-N:])
-                        eval_line(line)
-                        break
+                    if len(fetched_lines) >= N or f.tell() == 0: 
+                            # print() 
+                            line = ''.join(fetched_lines[-N:])
+                            self.eval_line(line)
+                            break
 
 if __name__ == '__main__': 
       
     fname = 'access.log'
     N = 1
+    cu = Check_url()
       
     while True:
         try: 
-            find_last(fname, 1)
-            break;
-        except: 
-            print('File not found')
+            cu.find_last(fname, 1)
+            # print(cu.gtime)
+            break
+        except Exception as e : 
+            print(e)
+            break
